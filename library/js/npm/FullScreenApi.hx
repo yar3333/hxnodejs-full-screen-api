890c12e860d4b61da64809a36d88bc68f880526f
Yaroslav Sivakov <yar3333@gmail.com>
2017-08-29 17:34:58 +0300

    + init

---
+++ library/js/npm/FullScreenApi.hx
@@ -0,0 +1,12 @@
+package js.npm;
+
+@:require("full-screen-api")
+extern class FullScreenApi
+{
+	public static var supportsFullScreen(default, null) : Bool;
+	public static var fullScreenEventName(default, null) : String;
+	
+	public static function isFullScreen() : Bool;
+	public static function requestFullScreen(el:js.html.Element) : Bool;
+	public static function cancelFullScreen() : Void;
+}
